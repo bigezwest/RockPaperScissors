cf2b65a15f845ae76935a87a050c94a45f7b23a3
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-06-17 16:51:57 +0200

    chore(comments): Update comments to show completed

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@
 /**
  * Instructions:
  *  X - 1. Create a new function named getComputerChoice.
-    2. Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+    X - 2. Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
         Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1. Think about how you can use this to conditionally return one of the multiple choices.
-    3. Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step.
+    X - 3. Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step.
  */
 function getComputerChoice() {
     const min = 1;
